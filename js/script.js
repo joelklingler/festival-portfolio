d34aeefda8e6813a9b75b66ef8f5894668403a69
Joèl Klingler <joel.klingler@hotmail.com>
2017-02-14 16:54:04 +0100

    added links and smooth scrolling
    Added links to scroll-info and nav-bar. Implemented smooth scrolling.

--- js/script.js
+++ js/script.js
@@ -16,7 +16,13 @@ $( document ).ready(function() {
         });
     });
 
+    $(document).on('click', 'a', function(event) {
+        event.preventDefault();
 
+        $('html,body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 100
+        }, 500);
+    });
 });
 
 function initMap() {
