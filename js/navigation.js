a17f792ab1bb7e7eecb7c53881a3e001333aaeb7
Luca Huber <luca.huber97@gmx.ch>
2017-02-15 11:08:33 +0100

    SUBERI SCHEISSE

--- js/navigation.js
+++ js/navigation.js
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 
-    var nav = $('nav');
-    nav.sticky( {topSpacing:0} );
+    $('nav').sticky( {topSpacing:0} );
 
-    $(window).scroll(function(){
-        var windscroll = $(window).scrollTop();
-
-        $('section').each(function(i) {
-            if ($(this).position().top <= windscroll + 100) {
-                $('nav li.active').removeClass('active');
-                $('nav li').eq(i).addClass('active');
-            }
-        });
-    });
-
-    $(document).on('click', 'a.scroll', function(event) {
-        event.preventDefault();
-
-        $('html,body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 100
-        }, 500);
-    });
-=======
-$(document).ready(function() {
     $(window).scroll(function(){
         var windscroll = $(window).scrollTop();
 
@@ -53,10 +31,9 @@ $(document).ready(function() {
 
     $("nav .overlay .overlay-content li a").click(function() {
         closeNavigationOverlay();
-    })
+    });
 
     function closeNavigationOverlay() {
         $("nav .overlay")[0].style.width = "0";
     }
->>>>>>> 91b517eae82f51f3845cd4257d7df6b048d996ab
 });
