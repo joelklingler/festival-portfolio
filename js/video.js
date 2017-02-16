ff758e115396fdae88b60608b4b657fe9fa8f96c
Joèl Klingler <joel.klingler@hotmail.com>
2017-02-16 21:34:17 +0100

    removed autostop after 6s #11

--- js/video.js
+++ js/video.js
@@ -28,10 +28,10 @@ event.target.playVideo();
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-}
+    /*if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }*/
 }
 function stopVideo() {
     player.stopVideo();
