9de5a5eaafaae8ed4adf7617f3ec2b067c4deb7c
Joèl Klingler <joel.klingler@hotmail.com>
2017-02-16 22:02:25 +0100

    moved files, added own video, play/pause

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-$( document ).ready(function() {
-
+$(document).ready(function() {
     var animatedLogo = $('.animated-logo');
     var bars = $('.pseudo-bars .bar');
     var barsAnimationStopped = false;
     var scene = $("#scene");
-    
-    toggleLogoAnimation();
+    var video = document.getElementById("bg-video"); 
 
+    // Initialise
     scene.parallax();
+
+    // Video
+    toggleLogoAnimation();
+    video.pause();
+
+    // Logo
     animatedLogo.on('click', function() {
         toggleLogoAnimation();
     });
-
     function toggleLogoAnimation() {
         toggleVideoFilter();
         if(barsAnimationStopped) {
             barsAnimationStopped = false;
             bars.css("-webkit-animation-play-state", "running");
+            // Play video:
+            video.play();
         } else {
             barsAnimationStopped = true;
             bars.css("-webkit-animation-play-state", "paused");
+            // Pause video:
+            video.pause();
         }
     };
-
     function toggleVideoFilter() {
-        $("section.landing-page .bg-media iframe").toggleClass("gray-scale-filter", 3000);
+        $("section.landing-page .bg-media video").toggleClass("gray-scale-filter", 3000);
     };
 });
