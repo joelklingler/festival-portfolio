ce0387e48baa912170ce6a324ef807b63c615aaf
Joèl Klingler <joel.klingler@hotmail.com>
2017-02-17 10:32:41 +0100

    added opacity for logo

--- js/script.js
+++ js/script.js
@@ -4,35 +4,39 @@ $(document).ready(function() {
     var barsAnimationStopped = false;
     var scene = $("#scene");
     var video = document.getElementById("bg-video");
-    var bgImg = $("section.landing-page .bg-media video");
+    var bgVideo = $("section.landing-page .bg-media video");
 
     // Initialise
     scene.parallax();
     centerBackgroundMedia();
     toggleLogoAnimation();
-    //video.muted = true;
+    animatedLogo.toggleClass("opacity-filter");
+    animatedLogo.toggleClass("gray-scale-filter");
+    video.muted = true;
 
     // Logo
     animatedLogo.on('click', function() {
         toggleLogoAnimation();
     });
     function toggleLogoAnimation() {
+        animatedLogo.toggleClass("opacity-filter");
+        animatedLogo.toggleClass("gray-scale-filter");
         toggleVideoFilter();
         if(barsAnimationStopped) {
             barsAnimationStopped = false;
             bars.css("-webkit-animation-play-state", "running");
             // Play unmuted video:
-            //video.muted = false;
+            video.muted = false;
         } else {
             barsAnimationStopped = true;
             bars.css("-webkit-animation-play-state", "paused");
             // Mute video:
-            //video.muted = true;
+            video.muted = true;
         }
     };
 
     function toggleVideoFilter() {
-        $("section.landing-page .bg-media video").toggleClass("gray-scale-filter", 3000);
+        $("section.landing-page .bg-media video").toggleClass("gray-scale-filter");
     };
 
     // Landing-page background
@@ -41,10 +45,10 @@ $(document).ready(function() {
     });
 
     function centerBackgroundMedia() {
-        bgImg.css({
+        bgVideo.css({
             position:'absolute',
-            left: ($(window).width() - bgImg.outerWidth())/2,
-            top: ($(window).height() - bgImg.outerHeight())/2
+            left: ($(window).width() - bgVideo.outerWidth())/2,
+            top: ($(window).height() - bgVideo.outerHeight())/2
         });
     }
 });
