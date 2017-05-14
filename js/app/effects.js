0ccd27f49471057e15d3bdd5fdfcc1754d525f75
Joèl Klingler <joel.klingler@hotmail.com>
2017-05-14 21:45:16 +0200

    added images

--- js/app/effects.js
+++ js/app/effects.js
@@ -68,8 +68,23 @@ $(document).ready(function() {
         }
     });
 
+    $(".block img").mouseover(function(e) {
+        var oldSrc = $(e.target).attr('src');;
+        var pos = oldSrc.lastIndexOf('1');
+        var newSrc = oldSrc.substring(0, pos) + '2' + oldSrc.substring(pos+1);
+        $(e.target).attr('src', newSrc);
+    });
+
+    $(".block img").mouseleave(function(e) {
+        var oldSrc = $(e.target).attr('src');;
+        var pos = oldSrc.lastIndexOf('2');
+        var newSrc = oldSrc.substring(0, pos) + '1' + oldSrc.substring(pos+1);
+        console.log(newSrc);
+        $(e.target).attr('src', newSrc);
+    });
+
     // Parallax scroll
-    images = ["http://edmsauce.wpengine.netdna-cdn.com/wp-content/uploads/2014/11/1366.jpg", "https://ultrajapan.com/wp-content/uploads/2015/11/japan-og-2.jpg", "http://hardstylemag.com/wp-content/uploads/2015/07/Defqon.1-2015-Mainstage-HardstyleMag.jpg"]
+    images = ["https://ultrajapan.com/wp-content/uploads/2015/11/japan-og-2.jpg", "http://hardstylemag.com/wp-content/uploads/2015/07/Defqon.1-2015-Mainstage-HardstyleMag.jpg"]
 
     var containers = $(".parallax-image-container");
     containers.each(function(index) {
